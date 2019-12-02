da420d82c38a1197574a753db7a9ad6d208eb558
sanjeev <sanjeevgupta9223@gmail.com>
2019-12-02 18:10:06 -0500

    button login

--- app/src/main/java/com/midtermtest/Activity/LoginActivity.java
+++ app/src/main/java/com/midtermtest/Activity/LoginActivity.java
@@ -52,6 +52,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         switch (view.getId()) {
 
+            case R.id.but_login:
+
+        }
+
         }
 
 
