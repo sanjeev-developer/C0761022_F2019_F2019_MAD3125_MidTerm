239d010900b64d5442bfbda8020e6f4685eaead1
sanjeev <sanjeevgupta9223@gmail.com>
2019-12-02 18:15:25 -0500

    layout commit

--- app/src/main/java/com/midtermtest/Activity/Splash.java
+++ app/src/main/java/com/midtermtest/Activity/Splash.java
@@ -9,4 +9,10 @@ import com.midtermtest.R;
 public class Splash extends AppCompatActivity {
     private static final String TAG = "Splash";
 
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
