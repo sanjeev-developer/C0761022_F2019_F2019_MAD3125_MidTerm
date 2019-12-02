a458c69dcac5c2829f563e39f9cff53949538bb4
sanjeev <sanjeevgupta9223@gmail.com>
2019-12-02 18:14:18 -0500

    Splash activity commit

--- app/src/main/java/com/midtermtest/Activity/Splash.java
+++ app/src/main/java/com/midtermtest/Activity/Splash.java
@@ -7,29 +7,3 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.midtermtest.R;
 
 public class Splash extends AppCompatActivity {
-    private static final String TAG = "Splash";
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        Thread Background = new Thread() {
-            public void run() {
-
-                try {
-                    sleep(800);
-                    finish();
-                    Intent intent = new Intent(Splash.this, LoginActivity.class);
-                    startActivity(intent);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        Background.start();
-    }
-}
-
