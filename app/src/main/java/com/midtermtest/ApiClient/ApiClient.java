761b36d35f49169e017108124c4a06d4eace7b5a
sanjeev <sanjeevgupta9223@gmail.com>
2019-12-02 18:23:17 -0500

    committ API

--- app/src/main/java/com/midtermtest/ApiClient/ApiClient.java
+++ app/src/main/java/com/midtermtest/ApiClient/ApiClient.java
@@ -13,30 +13,6 @@ public class ApiClient {
     public static final String BASE_URL = "http://www.mocky.io/v2/";
     private static Retrofit retrofit = null;
 
-
     public static Retrofit getRetrofit() {
-        if (retrofit == null) {
-
-            Gson gson = new GsonBuilder()
-                    .setLenient()
-                    .create();
-
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .client(getClient())
-                    .build();
-        }
-        return retrofit;
-    }
-
-    private static OkHttpClient getClient() {
-        return new OkHttpClient.Builder()
-                .connectTimeout(2, TimeUnit.MINUTES)
-                .readTimeout(2, TimeUnit.MINUTES)
-                .writeTimeout(2, TimeUnit.MINUTES)
-                .build();
-    }
-}
 
 
